d4c8e00c965c1bd70651298ccb69977e1ce8a6ff
dixxanta08 <dixantashrestha08@gmail.com>
2026-01-08 19:17:53 +0545

    push

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+// Serve static files (HTML, CSS, JS) from the current directory
+app.use(express.static(__dirname));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.use(cors());
 
